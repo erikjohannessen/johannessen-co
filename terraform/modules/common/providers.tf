cbac08c04e04c71c7a697cfec2603f601056e663
erik johannessen <erik.johannessen@gmail.com>
2026-07-31 08:54:11 -0700

    add test provider

--- terraform/modules/common/providers.tf
+++ terraform/modules/common/providers.tf
@@ -12,6 +12,19 @@ terraform {
 }
 
 provider "aws" {
+  alias = "test"
+
+  region = var.aws_region
+
+  default_tags {
+    tags = {
+      terraform   = "true"
+      environment = "test"
+    }
+  }
+}
+
+provider "aws" {
   region = var.aws_region
 
   default_tags {
