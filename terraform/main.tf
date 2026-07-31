f0f7fce3a19a40a45684893d7a765a9b902456d0
Erik Johannessen <ejohannessen@cal.berkeley.edu>
2026-07-31 13:47:57 -0700

    Move common resources into a separate plan and workspace (#28)
    * Move common resources into a separate plan and workspace
    
    * split PR workflow into multiple jobs
    
    * add test provider
    
    * Switch to referencing ghost_site submodule directly
    
    * Refactor push workflow to match updated PR workflow
    
    * Remove common module, no longer needed
    
    * Adjustments based on Code Review feedback
    
    ---------
    
    Co-authored-by: erik johannessen <erik.johannessen@gmail.com>

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,17 +22,3 @@ resource "aws_iam_instance_profile" "ssm_tunnel" {
   name = "ghost-ssm-tunnel-instance-profile"
   role = aws_iam_role.ssm_tunnel.name
 }
-
-module "ghost_site" {
-  source = "./modules/ghost_site"
-
-  environment                 = var.environment
-  route53_zone_name           = var.route53_zone_name
-  subdomain                   = var.subdomain
-  aws_region                  = var.aws_region
-  db_password                 = var.db_password
-  ghost_image                 = var.ghost_image
-  ssm_tunnel_instance_type    = var.ssm_tunnel_instance_type
-  ssm_tunnel_instance_profile = aws_iam_instance_profile.ssm_tunnel.name
-  local_dev_ip                = var.local_dev_ip
-}
